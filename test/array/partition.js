be808f798dca4e394f286fea28cd4a0cf35e47c1
Kasper Isager <kasperisager@gmail.com>
2017-03-26 21:21:04 +0200

    Pre-size `filter()` and `partition()` arrays (#24)

--- test/array/partition.js
+++ test/array/partition.js
@@ -2,10 +2,10 @@ import test from 'ava';
 import {partition} from '../../lib/array/partition';
 
 test('partition() partitions items of an array by predicate', async t => {
-  const a = [1, 2, 3, 4];
+  const a = [1, 2, 3, 4, 5];
   const p = partition(a, n => n % 2 === 0);
 
-  t.deepEqual(p, [[2, 4], [1, 3]]);
+  t.deepEqual(p, [[2, 4], [1, 3, 5]]);
 });
 
 test('partition() returns empty partitions if no array is given', async t => {
